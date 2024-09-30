593796d43499eeca375d8db63266db6a29316c6c
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-09-30 19:09:11 +0300

    checking working

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+const tg = window.Telegram.WebApp;
+
 
 const Main = () => {
+
+    useEffect(() => {
+        tg.ready()
+    }, []);
+
+    const closeApp = () => {
+        tg.close();
+    }
+
     return (
-        <></>
+        <>
+            work
+            <button onClick={closeApp}>Закрыть</button>
+        </>
     )
 };
 
